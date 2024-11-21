0eeadd0d50018fc2c12da9cbb46c151f569c7220
孙建钦 <jianqin.sun@daoyitong.com>
2024-11-21 13:31:14 +0800

    将文件移动到根目录

--- cursorLearn/config.js
+++
@@ -1,8 +0,0 @@
-// 从环境变量加载配置
-const CONFIG = {
-    API_KEY: process.env.API_KEY,
-    API_URL: process.env.API_URL,
-    STABILITY_API_KEY: process.env.STABILITY_API_KEY,
-    CLAUDE_API_KEY: process.env.CLAUDE_API_KEY,
-    CLAUDE_API_URL: process.env.CLAUDE_API_URL
-}
